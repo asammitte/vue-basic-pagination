10714660e06a7b0e2287be555c90bef809c34e83
Kostya <pavli4enko30@gmail.com>
2023-06-08 08:59:53 +0200

    add istanbul code coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,10 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      coverage: {
+        provider: 'istanbul',
+        reporter: ['text', 'json'],
+      },
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url)),
