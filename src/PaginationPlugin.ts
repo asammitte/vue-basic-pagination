79ecd1cf43687148526cea9b160dc1f0d95aeb30
Kostya <pavli4enko30@gmail.com>
2023-06-16 19:31:32 +0200

    remove commented code

--- src/PaginationPlugin.ts
+++ src/PaginationPlugin.ts
@@ -3,10 +3,8 @@ import { BasicPagination } from "./components"
 
 export default {
   install: (app: App) => {
-  // install: (app: App, options: { img: string } = { img: "" }) => {
     app.component("BasicPagination", BasicPagination)
-    // app.provide("specialUploadImage", options.img);
-  },
+  }
 }
 
 export { BasicPagination }
